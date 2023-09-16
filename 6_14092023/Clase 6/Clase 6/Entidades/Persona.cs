144ef7c5974601c12ff01e25a7d976177f2151a8
LMFT <67986002+LMFT@users.noreply.github.com>
2023-09-16 14:59:13 -0300

    Clase 6 subida

---
+++ 6_14092023/Clase 6/Clase 6/Entidades/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace Entidades
+{
+    public class Persona
+    {
+        private int dni;
+        private string nombre;
+        private string apellido;
+
+        public Persona(int dni, string nombre, string apellido)
+        {
+            this.dni = dni;
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Nombre: {this.nombre}");
+            sb.AppendLine($"Apellido: {this.apellido}");
+            sb.AppendLine($"DNI: {this.dni}");
+            return sb.ToString();
+        }
+    }
+}
