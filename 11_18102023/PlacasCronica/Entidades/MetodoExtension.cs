3662b236b263dc70cf605c458f75d5a56bb5ad9b
Alejandro Bongioanni <a.bongioanni@sistemas-utnfra.com.ar>
2023-10-18 22:24:57 -0300

    Placas Cronica

---
+++ 11_18102023/PlacasCronica/Entidades/MetodoExtension.cs
@@ -0,0 +1,36 @@
+﻿namespace Entidades
+{
+    public enum EEstaciones { Verano = 12, Otonio = 3, Invierno = 7, Primavera = 9 };
+
+    public static class MetodoExtension
+    {
+        public static string ObtenerPlacaCronicaAle(this DateTime fecha, EEstaciones estaciones)
+        {
+            int anio = 2023;
+            int mes = (int)estaciones;
+            if(fecha.Month > mes)
+            {
+                anio = 2024;
+            }
+
+            DateTime fechaSegunEstacion = new DateTime(anio, (int)estaciones, 21);
+            TimeSpan lapsoDeTiempo = fechaSegunEstacion - fecha;
+
+            return $"Falta {lapsoDeTiempo.TotalDays.ToString("0")} para el {estaciones.ToString()}";
+        }
+
+        public static string ObtenerPlacaCronicaLucas(this DateTime fecha, EEstaciones e)
+        {
+            int diasFecha = fecha.DayOfYear;
+            int diasDelAnio = new DateTime(fecha.Year, (int)e, 21).DayOfYear;
+
+            if (diasFecha > diasDelAnio)
+            {
+                diasDelAnio += 365;
+            }
+
+            return $"Faltan {diasDelAnio - diasFecha} para el {Enum.GetName(e)}";
+        }
+
+    }
+}
---
+++ 11_18102023/PlacasCronica/PlacasCronica/Program.cs
@@ -0,0 +1,15 @@
+﻿using Entidades;
+namespace PlacasCronica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime ahora = new DateTime(2023, 3, 1);
+
+
+            Console.WriteLine(ahora.ObtenerPlacaCronicaAle(EEstaciones.Verano));
+            Console.WriteLine(ahora.ObtenerPlacaCronicaLucas(EEstaciones.Verano));
+        }
+    }
+}
