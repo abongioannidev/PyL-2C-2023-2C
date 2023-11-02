466191d65dd149280fc2b19b2cb93c347be22cdd
Alejandro Bongioanni <a.bongioanni@sistemas-utnfra.com.ar>
2023-11-01 22:10:40 -0300

    bd

---
+++ 16_01112023/Clase16/Clase16/Program.cs
@@ -0,0 +1,14 @@
+﻿using Entidades;
+namespace Clase16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GestorSql.GetDatosPersona(10));
+
+            Console.WriteLine("*******************************************");
+            GestorSql.GetAllPersonas();
+        }
+    }
+}
---
+++ 16_01112023/Clase16/Entidades/GestorSql.cs
@@ -0,0 +1,81 @@
+﻿using System.Data.SqlClient;
+namespace Entidades
+{
+    public static class GestorSql
+    {
+        private static string stringConnection;
+
+        static GestorSql()
+        {
+            GestorSql.stringConnection = "Server=.;Database=clase01112023;Trusted_Connection=True;";
+        }
+
+
+
+        public static string GetDatosPersona(int id)
+        {
+            SqlConnection connection = new SqlConnection(GestorSql.stringConnection);
+
+            string sentencia = $"SELECT idPersona,nombre,apellido FROM PERSONAS WHERE idPersona={id}";
+
+            try
+            {
+                SqlCommand command = new SqlCommand(sentencia, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader(); 
+          
+                if(reader.Read())
+                {
+                    return $"Persona con ID {reader.GetInt32(0)} =>{reader.GetString(1)},{reader.GetString(2)}";
+                }
+                return "No encotre un nombre para el ID RECIBIDO";
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+            finally
+            {
+                if(connection != null && connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+            
+        }
+
+
+        public static void GetAllPersonas()
+        {
+            SqlConnection connection = new SqlConnection(GestorSql.stringConnection);
+
+            string sentencia = $"SELECT * FROM PERSONAS";
+
+            try
+            {
+                SqlCommand command = new SqlCommand(sentencia, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+           
+                while(reader.Read()) 
+                {
+                    Console.WriteLine($"Persona con ID {reader.GetInt32(0)} =>{reader.GetString(1)},{reader.GetString(2)}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (connection != null && connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+
+        }
+
+
+    }
+}
